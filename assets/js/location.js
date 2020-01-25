9376f0352fa6634a72021df54479a8e95a8fd9eb
imeuro <mauro.fioravanzi@gmail.com>
2020-01-25 01:51:37 +0100

    cssini & co

--- assets/js/location.js
+++ assets/js/location.js
@@ -1,59 +1,55 @@
 var button = document.getElementById('getCons');
+var startPos;
+var nudge = document.getElementById("nudge");
 
-// preflight check: Geolocation support
-if (navigator.geolocation) {
-  console.log('Geolocation is supported!');
+var showNudgeBanner = function() { nudge.style.display = "block"; };
+var hideNudgeBanner = function() { nudge.style.display = "none"; };
+
+var geoSuccess = function(position) {
+  // Do magic with location
+  startPos = position;
+  // document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+  // document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+  nudge.innerHTML = nudge.innerHTML+ '<br>Position acquired: '+startPos.coords.latitude+', '+startPos.coords.longitude;
+};
+var geoError = function(error) {
+  switch(error.code) {
+    case error.TIMEOUT:
+      // The user didn't accept the callout
+      showNudgeBanner();
+      break;
+  }
+  nudge.innerHTML = nudge.innerHTML+ '<br>Error occurred. Error code: ' + error.code;
+};
+
+// preflight check: Geolocation support & BT capabilities
+if (navigator.geolocation && navigator.bluetooth) {
+  nudge.innerHTML = 'Cool, geolocation is supported!';
 
   button.onclick = function() {
-    var startPos;
-    var nudge = document.getElementById("nudge");
-
-    var showNudgeBanner = function() {
-      nudge.style.display = "block";
-    };
-
-    var hideNudgeBanner = function() {
-      nudge.style.display = "none";
-    };
-
-    var nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
-
-    var geoSuccess = function(position) {
-      hideNudgeBanner();
-      // We have the location, don't display banner
-      clearTimeout(nudgeTimeoutId); 
-
-      // Do magic with location
-      startPos = position;
-      document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-      document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-    };
-    var geoError = function(error) {
-      switch(error.code) {
-        case error.TIMEOUT:
-          // The user didn't accept the callout
-          showNudgeBanner();
-          break;
-      }
-      console.log('Error occurred. Error code: ' + error.code);
-    };
 
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-
 
     // now look for BT devices:
     navigator.bluetooth.requestDevice({
       acceptAllDevices: true,
       optionalServices: ['battery_service']
     })
-    .then(device => { /* ... */ })
-    .catch(error => { console.log(error); });
-
-
-    
+    .then(device => {       
+      nudge.innerHTML = nudge.innerHTML+ '<br>device found: '+device;
+      /* ... */ 
+    })
+    .catch(error => { 
+      console.log(error);
+      nudge.innerHTML = nudge.innerHTML+ '<br>Error occurred: '+error;
+    });
   };
 
 }
 else {
-  console.log('Geolocation is not supported for this Browser/OS.');
+  if (!navigator.geolocation)
+    nudge.innerHTML = 'Geolocation is not supported for this Browser.';
+  if (!navigator.bluetooth)
+    nudge.innerHTML = 'BT is not supported for this Browser.';
+
 }
